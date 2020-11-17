411628eedd650efe65ed76b16056698157687335
ItamarAlves <itamaralvesprofi@gmail.com>
2020-11-17 01:37:47 -0400

    add pesquisaEstado()

--- backend/src/app/controllers/DashboardController.js
+++ backend/src/app/controllers/DashboardController.js
@@ -72,12 +72,12 @@ class DashboardController {
 
                 var valueTotal = 0;
 
-                for (var x = 0; x < dataRes.data.length; x++) {
-                    var percPalavra = Math.round((dataRes.data[x] / valueTotal) * 100, 2);
-                    dataRes.data[x] = percPalavra;
-                }
+                // for (var x = 0; x < dataRes.data.length; x++) {
+                //     var percPalavra = Math.round((dataRes.data[x] / valueTotal) * 100, 2);
+                //     dataRes.data[x] = percPalavra;
+                // }
 
-                return response.status(200).json(dataRes);
+                return response.status(200).json(dataResponse);
             } else {
                 return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
             };
@@ -85,6 +85,29 @@ class DashboardController {
     }
 
     async pesquisaEstado(request, response) {
+        const palavra = request.query.palavra;
+        const top = request.query.top;
+        const parameters = { keyword: palavra, geo: 'BR', resolution: 'region', hl: "pt-br" }
+
+        await googleTrends.interestByRegion(parameters, function (err, results) {
+            if (!err) {
+                var dataResponse = results.toString();
+                dataResponse = JSON.parse(dataResponse);
+                const dataRes = {
+                    "labels": [],
+                    "data": []
+                };
+
+                for (var i = 0; i < top; i++) {
+                    dataRes.labels.push(dataResponse.default.geoMapData[i].geoName);
+                    dataRes.data.push(dataResponse.default.geoMapData[i].value)
+                }
+
+                return response.status(200).json(dataRes);
+            } else {
+                return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
+            };
+        });
 
     }
 }

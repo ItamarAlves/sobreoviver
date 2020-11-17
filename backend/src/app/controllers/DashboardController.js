51667493b9d4dc92100bfb0355612baca37cedf3
ItamarAlves <itamaralvesprofi@gmail.com>
2020-11-17 00:21:44 -0400

    add comparePalavra(), Regiao.json

--- backend/src/app/controllers/DashboardController.js
+++ backend/src/app/controllers/DashboardController.js
@@ -4,48 +4,57 @@ import palavraList from '../palavra/Palavra.json'
 class DashboardController {
 
     async comparePalavra(request, response) {
-        const palavra = request.query.palavra;
-
-        if (palavra != null) {
-
-            const parameters = { keyword: palavra, geo: 'BR', resolution: 'region', hl: "pt-br" }
-
-            googleTrends.interestByRegion(parameters, function (err, results) {
-                if (!err) {
-                    var dataResponse = results.toString();
-                    dataResponse = JSON.parse(dataResponse);
-                    const dataRes = {
-                        "labels": [],
-                        "data": []
-                    };
-                    
-                    for (var p = 0; p < parameters.keyword.length; p++) {
-                        dataRes.labels.push(parameters.keyword[p]);
-                        for (var i = 0; i < dataResponse.default.geoMapData.length; i++) {
-                            for (var y = 0; y < dataResponse.default.geoMapData[i].value.length; y++) {
-                                console.log(dataRes.data.length);
-                                dataRes.data.push(dataRes.data.length > 0 ? dataRes.data[p] : 0 + dataResponse.default.geoMapData[i].value[p]);
-                            }
-                        }
-                    }
-                    return response.status(200).json(dataRes);
-                } else {
-                    return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
-                };
-
-            });
-
-        } else {
-            console.log(palavraList.palavra)
-            for (var i = 0; i < palavraList.palavra.length; i++) {
-                const parameters = { keyword: palavraList.palavra[i] }
-                result = consultApi(response, parameters);
-
-            }
-
-            return null;
+        const palavraArray = {
+            "palavra": []
         }
 
+        const palavra = request.query.palavra;
+
+        if (palavra != null && palavra != '') {
+            palavraArray.palavra.push(palavra)
+        } else {
+            for (var i = 0; i < palavraList.palavra.length; i++) {
+                palavraArray.palavra.push(palavraList.palavra[i]);
+            }
+        }
+
+        const parameters = { keyword: palavraArray.palavra, geo: 'BR', resolution: 'region', hl: "pt-br" }
+
+        await googleTrends.interestByRegion(parameters, function (err, results) {
+            if (!err) {
+                var dataResponse = results.toString();
+                dataResponse = JSON.parse(dataResponse);
+                const dataRes = {
+                    "labels": [],
+                    "data": []
+                };
+                console.log(parameters.keyword)
+                console.log(parameters.keyword.length)
+                var valueTotal = 0;
+                for (var p = 0; p < parameters.keyword.length; p++) {
+                    var value = 0;
+                    dataRes.labels.push(parameters.keyword[p]);
+                    for (var i = 0; i < dataResponse.default.geoMapData.length; i++) {
+                        for (var y = 0; y < dataResponse.default.geoMapData[i].value.length; y++) {
+                            if (y != p) continue;
+                            value += dataResponse.default.geoMapData[i].value[p];
+                        }
+                    }
+                    valueTotal += value;
+                    dataRes.data.push(value);
+                }
+
+                for (var x = 0; x < dataRes.data.length; x++) {
+                    var percPalavra = Math.round((dataRes.data[x] / valueTotal) * 100, 2);
+                    dataRes.data[x] = percPalavra;
+                }
+
+                return response.status(200).json(dataRes);
+            } else {
+                return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
+            };
+
+        });
     }
 
     async pesquisaRegiao(request, response) {
@@ -56,20 +65,5 @@ class DashboardController {
 
     }
 }
-
-async function consultApi(response, parameters) {
-    googleTrends.interestByRegion(parameters, function (err, results) {
-        if (!err) {
-            var dataResponse = results.toString();
-            dataResponse = JSON.parse(dataResponse);
-
-            return response.status(200).json(dataResponse);
-        } else {
-            return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
-        };
-
-    });
-}
-
 
 export default new DashboardController();

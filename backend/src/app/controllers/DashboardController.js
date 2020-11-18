fe6c6718227a2237aae9d9b2eb9ac77373e443cc
ItamarAlves <itamaralvesprofi@gmail.com>
2020-11-18 04:20:33 -0400

    ajustando pesquisaRegiao()

--- backend/src/app/controllers/DashboardController.js
+++ backend/src/app/controllers/DashboardController.js
@@ -1,6 +1,7 @@
 import googleTrends from 'google-trends-api';
 import palavraList from '../palavra/Palavra.json'
 import regiaoList from '../regiao/Regiao.json'
+import regiaoEstadoList from '../regiao/RegiaoEstado.json'
 
 class DashboardController {
 
@@ -78,16 +79,29 @@ class DashboardController {
                 };
 
                 var valueTotal = 0;
-                for (var r = 0; r < regiaoList.length; r++) {
+                for (var t = 0; t < regiaoList.regiao.length; t++) {
+                    var regiao = regiaoList.regiao[t];
+                    dataRes.labels.push(regiao);
 
+                    var value = 0
+                    for (var i = 0; i < regiaoEstadoList[regiao].length; i++) {
+                        var estado = regiaoEstadoList[regiao][i];
+                        for (var y = 0; y < dataResponse.default.geoMapData.length; y++) {
+                            if (dataResponse.default.geoMapData[y].geoName == estado) {
+                                value += dataResponse.default.geoMapData[y].value[0];
+                            };
+                        }
+                    }
+                    valueTotal += value;
+                    dataRes.data.push(value)
                 }
 
-                // for (var x = 0; x < dataRes.data.length; x++) {
-                //     var percPalavra = Math.round((dataRes.data[x] / valueTotal) * 100, 2);
-                //     dataRes.data[x] = percPalavra;
-                // }
+                for (var x = 0; x < dataRes.data.length; x++) {
+                    var percRegiao = Math.round((dataRes.data[x] / valueTotal) * 100, 2);
+                    dataRes.data[x] = percRegiao;
+                }
 
-                return response.status(200).json(dataResponse);
+                return response.status(200).json(dataRes);
             } else {
                 return response.status(500).json({ "error": "Tente efetuar a solicitação novamente em alguns minutos, por gentileza." });
             };
@@ -110,7 +124,9 @@ class DashboardController {
 
                 for (var i = 0; i < top; i++) {
                     dataRes.labels.push(dataResponse.default.geoMapData[i].geoName);
-                    dataRes.data.push(dataResponse.default.geoMapData[i].value)
+                    for (var y = 0; y < dataResponse.default.geoMapData[i].value.length; y++) {
+                        dataRes.data.push(dataResponse.default.geoMapData[i].value[y]);
+                    }
                 }
 
                 return response.status(200).json(dataRes);

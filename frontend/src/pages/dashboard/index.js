c7f763dd39bcb7eab4f92c0a195f4ceda7a6fc2a
ItamarAlves <itamaralvesprofi@gmail.com>
2020-11-17 23:25:00 -0400

    ajustando css input pesquisa

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -9,18 +9,18 @@ const Dashboard = () => {
     <div>
       <Menu />
       <h1 id="text-dashboard">Dashboard</h1>
-      <div class="input-group col-12 " id="pesquisaPalavra">
-        <form id="formPalavra" action={Charts} method="GET" >
-        <input class="form-control border-right-0" id="palavra"
-          placeholder="Digite aqui a palavra que deseja pesquisar" />
-        <span class="input-group-append bg-white">
-          <button class="btn border border-left-0" type="submit"><i class="fa fa-search"></i></button>
-        </span>
-        </form>
-      </div>
-      <div className="col-12">
-        <small id="emailHelp" class="form-text text-muted">Separe os termos por vírgula. Ex.: Termo 1, Termo 2, Termo 3.</small>
-      </div>
+      <form id="formPalavra" action={Charts} method="GET" >
+        <div class="input-group col-12 " id="pesquisaPalavra">
+          <input class="form-control border-right-0" id="palavra"
+            placeholder="Digite aqui a palavra que deseja pesquisar" />
+          <span class="input-group-append bg-white">
+            <button class="btn border border-left-0" type="submit"><i class="fa fa-search"></i></button>
+          </span>
+        </div>
+        <div className="col-12">
+          <small id="emailHelp" class="form-text text-muted">Separe os termos por vírgula. Ex.: Termo 1, Termo 2, Termo 3.</small>
+        </div>
+      </form>
       <div className="container-fluid">
         <div className="row">
           <Charts />
